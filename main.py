5c63f221f03cdc55686369b493b40e2e2d00c124
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2020-11-30 15:32:07 +0100

    add sending form data to server via xhr

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, jsonify
+import re
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+	res = "Received config:<br>"
+	res += request.form["bpp"].replace("\n", "<br>")
+	res += "<br>"
+	# .files alements are werkzeug FileStorage objects
+	for k in request.files:
+		res += f"<br>File: {k} {request.files[k].filename}"
+	return res
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- ts/dom.parse.ts
+++ ts/dom.parse.ts
@@ -2,7 +2,7 @@ function pushfile(sym: Sym, file: HTMLInputElement){
 	files[sym.parent.ref()] = file;
 }
 function showerrors(){
-	document.getElementById("errors")!.textContent = "No errors.";
+	//document.getElementById("errors")!.textContent = "No errors.";
 }
 function showcompiled(){
 	const r = document.getElementById("result") as HTMLDivElement;
@@ -15,9 +15,32 @@ function showcompiled(){
 	r.innerHTML += "<br>Files:<br>";
 	for(let k in files)
 		r.innerHTML += k + ": " + files[k].files![0].name + "<br>";
+	return s;
 }
 function submit(){
+	const fd = new FormData();
+	const s = showcompiled();
+	fd.append("bpp", s.join("\n"));
+	for(let k in files)
+		fd.append(k, files[k].files![0]);
 
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = () => {
+		if(xhr.readyState === XMLHttpRequest.DONE)
+			document.getElementById("response")!.innerHTML = xhr.responseText;
+	}
+	/*
+	xhr.addEventListener("load", (e: Event) => {
+		(e);
+		window.location.reload();
+	});
+	xhr.addEventListener("error", (e: Event) => {
+		(e);
+		alert("error");
+	});
+	*/
+	xhr.open("POST", "submit", true);
+	xhr.send(fd);
 }
 function registersubmit(){
 	const b = document.getElementsByTagName("body")[0];
