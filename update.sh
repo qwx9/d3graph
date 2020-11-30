3acad2b8e18b3d3324acdca6b3550d26611ad627
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2020-11-30 02:56:29 +0100

    reconfigure ts compilation to use a script
    concatenating via tsconfig requires lots more work for whatever reason

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+files=(\
+ts/fns.ts \
+ts/dom.expr.ts \
+ts/dom.option.ts \
+ts/dom.parse.ts \
+ts/dom.sym.ts \
+ts/dom.val.ts \
+ts/dom.ts \
+ts/expr.ts \
+ts/option.ts \
+ts/parse.ts \
+ts/rule.ts \
+ts/sym.ts \
+ts/val.ts \
+\
+ts/main.ts \
+)
+cat ${files[@]} >static/app.ts
+npx tsc
